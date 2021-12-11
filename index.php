ace8172f22461aff20a22521cf7f9f5ff36daafa
unknown <abbas30000@gmail.com>
2021-12-11 22:55:50 +0330

    more from oop

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
-
+//define a trait
+trait like {
+public function AddLike($likely) {
+    echo "you like  ".$likely.'<br>';
+  }
+}
 class Product {
+	use like;
     // Properties
     public $id;
     public $name;
@@ -33,12 +39,16 @@ class Product {
     //private func
     private function MyPrivateMethod()
     {
-        echo "This is a Private method"; 
+        echo "This is a Private method<br>"; 
     }
     //protected func
     protected function MyProtectMethod()
     {
-        echo "This is a Protected method"; 
+        echo "This is a Protected method<br>"; 
+    }  //protected func
+    final function MyFinalMethod()
+    {
+        echo "This is a final method<br>"; 
     } 
   //pubic function print protected & private property & function
     public function MyPrint()
@@ -49,10 +59,50 @@ class Product {
 
 }
 //new class extended
-class newProduct extends product {
-    //use protected method & property
-    
+class newProduct extends Product{
+//overrride function
+  function MyPrint()
+  {
+
+	 // use protected method
+	$this->MyProtectMethod();
+	 echo 'override myprint <br/>';
+  }
 }
+abstract class AbstractProduct
+{
+    abstract protected function MyPrint($name);
+	function Publicfunc()
+	{
+		echo 'abstract class have public method<br>';
+	}
+}
+
+class Digital extends AbstractProduct
+{
+
+    public function MyPrint($name)
+	{
+       echo $name.' a Digital production<br>';
+    }
+}
+
+interface DigitalProduct {
+  public function Brand();
+}
+interface mobile{
+  public function HaveCamera();
+}
+class applemobile implements DigitalProduct,mobile {
+  public function Brand() {
+    echo " apple <br>";
+  }
+  public function HaveCamera()
+  {
+	echo 'this mobile have camera <br>';  
+  }
+}
+
 //define new obj from product
 $mobile=new Product();
 //use setter
@@ -63,13 +113,22 @@ echo $mobile->get_name().'<br/>';
 echo $mobile->get_id().'<br/>';
 // use public function
 $mobile->MyPrint();
+//use trait
+$mobile->AddLike('iphne');
 
-// use public function
-$mobile->MyProtectMethod();
 //define new child obj 
 $newmobile=new newProduct;
-//use constant 
-echo $newmobile::protectProperty;
-
+//use override method 
+$newmobile->MyPrint();
+//define new child object from AbstractProduct
+$digital=new Digital;
+//use abstract method  
+$digital->MyPrint('laptop');
+//use functional method
+$digital->Publicfunc('laptop');
+//define new object from interface chid
+$iphone6 = new applemobile;
+$iphone6->Brand();
+$iphone6->HaveCamera();
 
 ?>
